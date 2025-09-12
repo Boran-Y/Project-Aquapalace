672b1f5178d76a32b4bb919b60ea31b56cacbfb4
Boran-Y <163313195+Boran-Y@users.noreply.github.com>
2025-09-12 11:18:53 +0200

    fix
    fix

--- eindProjectAquaPalace/FrmUsers.Designer.cs
+++ eindProjectAquaPalace/FrmUsers.Designer.cs
@@ -28,85 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.lbxOverzicht = new System.Windows.Forms.ListBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtGebruikersnaam = new System.Windows.Forms.TextBox();
+            this.txtWachtwoord = new System.Windows.Forms.TextBox();
+            this.txtRol = new System.Windows.Forms.TextBox();
+            this.txtGekoppeld = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.id = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lbxOverzicht
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(101, 100);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(538, 292);
-            this.listBox1.TabIndex = 0;
+            this.lbxOverzicht.FormattingEnabled = true;
+            this.lbxOverzicht.ItemHeight = 16;
+            this.lbxOverzicht.Location = new System.Drawing.Point(87, 101);
+            this.lbxOverzicht.Name = "lbxOverzicht";
+            this.lbxOverzicht.Size = new System.Drawing.Size(538, 292);
+            this.lbxOverzicht.TabIndex = 0;
+            this.lbxOverzicht.SelectedIndexChanged += new System.EventHandler(this.lbxOverzicht_SelectedIndexChanged);
             // 
-            // textBox1
+            // txtId
             // 
-            this.textBox1.Location = new System.Drawing.Point(742, 51);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 22);
-            this.textBox1.TabIndex = 1;
+            this.txtId.Location = new System.Drawing.Point(742, 196);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(195, 22);
+            this.txtId.TabIndex = 2;
             // 
-            // textBox2
+            // txtGebruikersnaam
             // 
-            this.textBox2.Location = new System.Drawing.Point(742, 110);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(195, 22);
-            this.textBox2.TabIndex = 2;
+            this.txtGebruikersnaam.Location = new System.Drawing.Point(742, 253);
+            this.txtGebruikersnaam.Name = "txtGebruikersnaam";
+            this.txtGebruikersnaam.Size = new System.Drawing.Size(195, 22);
+            this.txtGebruikersnaam.TabIndex = 3;
             // 
-            // textBox3
+            // txtWachtwoord
             // 
-            this.textBox3.Location = new System.Drawing.Point(742, 172);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(195, 22);
-            this.textBox3.TabIndex = 3;
+            this.txtWachtwoord.Location = new System.Drawing.Point(742, 304);
+            this.txtWachtwoord.Name = "txtWachtwoord";
+            this.txtWachtwoord.Size = new System.Drawing.Size(195, 22);
+            this.txtWachtwoord.TabIndex = 4;
             // 
-            // textBox4
+            // txtRol
             // 
-            this.textBox4.Location = new System.Drawing.Point(742, 230);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(195, 22);
-            this.textBox4.TabIndex = 4;
+            this.txtRol.Location = new System.Drawing.Point(742, 356);
+            this.txtRol.Name = "txtRol";
+            this.txtRol.Size = new System.Drawing.Size(195, 22);
+            this.txtRol.TabIndex = 5;
             // 
-            // textBox5
+            // txtGekoppeld
             // 
-            this.textBox5.Location = new System.Drawing.Point(742, 283);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(195, 22);
-            this.textBox5.TabIndex = 5;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(742, 345);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(195, 22);
-            this.textBox6.TabIndex = 6;
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(742, 406);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(195, 22);
-            this.textBox7.TabIndex = 7;
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(742, 468);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(195, 22);
-            this.textBox8.TabIndex = 8;
+            this.txtGekoppeld.Location = new System.Drawing.Point(742, 405);
+            this.txtGekoppeld.Name = "txtGekoppeld";
+            this.txtGekoppeld.Size = new System.Drawing.Size(195, 22);
+            this.txtGekoppeld.TabIndex = 6;
             // 
             // panel1
             // 
@@ -114,44 +99,126 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(0, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1016, 41);
+            this.panel1.Size = new System.Drawing.Size(1016, 58);
             this.panel1.TabIndex = 9;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.SteelBlue;
-            this.panel2.Location = new System.Drawing.Point(0, 44);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(81, 543);
-            this.panel2.TabIndex = 10;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(137, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.Size = new System.Drawing.Size(115, 22);
             this.label1.TabIndex = 0;
             this.label1.Text = "Aquapalace";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel2.Location = new System.Drawing.Point(0, 61);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(81, 526);
+            this.panel2.TabIndex = 10;
+            // 
+            // id
+            // 
+            this.id.AutoSize = true;
+            this.id.Location = new System.Drawing.Point(672, 201);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(18, 16);
+            this.id.TabIndex = 11;
+            this.id.Text = "id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(631, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "gebruikersnaam";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(658, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "wachtwoord";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(672, 362);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 16);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "rol";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(631, 408);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "gekoppeld klant";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(112, 485);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(265, 59);
+            this.btnCreate.TabIndex = 16;
+            this.btnCreate.Text = "voeg toe";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(425, 485);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(265, 59);
+            this.btnEdit.TabIndex = 17;
+            this.btnEdit.Text = "pas aan";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(719, 485);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(265, 59);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "verwijder";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // FrmUsers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1012, 588);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.id);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtGekoppeld);
+            this.Controls.Add(this.txtRol);
+            this.Controls.Add(this.txtWachtwoord);
+            this.Controls.Add(this.txtGebruikersnaam);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lbxOverzicht);
             this.Name = "FrmUsers";
             this.Text = "FrmUsers";
+            this.Load += new System.EventHandler(this.FrmUsers_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -161,17 +228,22 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.ListBox lbxOverzicht;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtGebruikersnaam;
+        private System.Windows.Forms.TextBox txtWachtwoord;
+        private System.Windows.Forms.TextBox txtRol;
+        private System.Windows.Forms.TextBox txtGekoppeld;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
--- eindProjectAquaPalace/FrmUsers.cs
+++ eindProjectAquaPalace/FrmUsers.cs
@@ -16,5 +16,97 @@ namespace eindProjectAquaPalace
         {
             InitializeComponent();
         }
+
+        private void lbxOverzicht_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbxOverzicht.SelectedItem != null)
+            {
+                Gebruikers gebruikers = new Gebruikers();
+                gebruikers = (Gebruikers)lbxOverzicht.SelectedItem;
+                txtGebruikersnaam.Text = gebruikers.Gebruikersnaam;
+                txtWachtwoord.Text = gebruikers.WachtwoordHash;
+                txtRol.Text = Convert.ToString(gebruikers.Rol);
+                if (gebruikers.GekoppeldKlant != null)
+                    txtGekoppeld.Text = Convert.ToString(gebruikers.GekoppeldKlant);
+                else
+                    txtGekoppeld.Text = "";
+
+            }
+
+
+
+        }
+
+        private void FrmUsers_Load(object sender, EventArgs e)
+        {
+            lbxOverzicht.Items.Clear();
+            foreach (Gebruikers UserObject in Gebruikers.Userlist())
+            {
+                lbxOverzicht.Items.Add(UserObject);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(lbxOverzicht.SelectedItems != null)
+            {
+                ((Gebruikers)lbxOverzicht.SelectedItem).Delete();
+
+            }
+            MessageBox.Show(lbxOverzicht.SelectedItem.ToString() + "is verwijderd");
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            int GebruikerId = char.Parse(txtId.Text);
+            string Gebruikersnaam = txtGebruikersnaam.Text;
+            string WachtwoordHash = txtWachtwoord.Text;
+            GebruikersRol Rol = (GebruikersRol)Enum.Parse(typeof(GebruikersRol), txtRol.Text);
+            int? GekoppeldKlant = null;
+
+            Gebruikers UserObject = new Gebruikers();
+            UserObject.GebruikerId = GebruikerId;
+            UserObject.Gebruikersnaam = Gebruikersnaam;
+            UserObject.WachtwoordHash = WachtwoordHash;
+            UserObject.Rol = Rol;
+            UserObject.GekoppeldKlant = GekoppeldKlant;
+            UserObject.Insert();
+            MessageBox.Show("Nieuwe gebruiker is aangemaakt");
+
+
+            txtId.Clear();
+            txtGebruikersnaam.Clear();
+            txtWachtwoord.Clear();
+            txtRol.Clear();
+            txtGekoppeld.Clear();
+           
+
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            int GebruikerId = char.Parse(txtId.Text);
+            string Gebruikersnaam = txtGebruikersnaam.Text;
+            string WachtwoordHash = txtWachtwoord.Text;
+            GebruikersRol Rol = (GebruikersRol)Enum.Parse(typeof(GebruikersRol), txtRol.Text);
+            int? GekoppeldKlant = null;
+
+            Gebruikers UserObject = new Gebruikers();
+            UserObject.GebruikerId = GebruikerId;
+            UserObject.Gebruikersnaam = Gebruikersnaam;
+            UserObject.WachtwoordHash = WachtwoordHash;
+            UserObject.Rol = Rol;
+            UserObject.GekoppeldKlant = GekoppeldKlant;
+            UserObject.Update();
+            MessageBox.Show("gebruiker is aangepast");
+
+
+            txtId.Clear();
+            txtGebruikersnaam.Clear();
+            txtWachtwoord.Clear();
+            txtRol.Clear();
+            txtGekoppeld.Clear();
+        }
     }
 }
